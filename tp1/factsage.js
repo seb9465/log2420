561467a71d8325f4a1b90c8c2803e7662ea8cd59
seb9465 <sebastiencadorette@Sansfil-Securise-Etudiants-Poly-255-28.polymtl.ca>
2017-09-06 13:01:48 -0400

    SC : Pushing things.

--- tp1/factsage.js
+++ tp1/factsage.js
@@ -1,10 +1,12 @@
-fonction loadDoc() {
+function loadDoc(){
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = fonction(){
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("corps").innerHTML = this.responseText;
+    
+    xhttp.onreadystatechange = function(){
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            document.getElementById('corpsint').innerHTML = this.responseText;
         }
     };
-    xhttp.open("GET", "www.factsage.com", true);
+
+    xhttp.open("GET", "http://www.factsage.com", true);
     xhttp.send();
 }
