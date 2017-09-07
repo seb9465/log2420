a89ac3cbbdfb2a0ad27a62e9d7cb9a01c164a409
seb9465 <cado.94@me.com>
2017-09-07 11:13:15 -0400

    SC : Solved AJAX struggle.

--- tp1/factsage.js
+++ tp1/factsage.js
@@ -2,11 +2,10 @@ function loadDoc(){
     var xhttp = new XMLHttpRequest();
     
     xhttp.onreadystatechange = function(){
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-            document.getElementById('corpsint').innerHTML = this.responseText;
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById('corps').innerHTML = this.responseText;
         }
     };
-
-    xhttp.open("GET", "http://www.factsage.com", true);
+    xhttp.open("GET", "fs_general.php", true);
     xhttp.send();
 }
