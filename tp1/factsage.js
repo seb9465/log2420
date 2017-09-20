d66d175221af3012da705ac94cb5f19f499099ce
unknown <andrei.catana@polymtl.ca>
2017-09-20 16:03:47 -0400

    button request changing to js file, added links for all buttons

--- tp1/factsage.js
+++ tp1/factsage.js
@@ -1,3 +1,5 @@
+	
+/* fait appel à une requette qui charge la page (change le contenu du tableau corp) */
 function loadDoc(url){
     var xhttp = new XMLHttpRequest();
     
@@ -10,3 +12,9 @@ function loadDoc(url){
     xhttp.open("GET", url, true);
     xhttp.send();
 }
+
+	/* fait l'appel à l'eveniment click afin d'appeler la fonction loadDoc qui affiche la page */
+
+	document.getElementById("reqGeneral").addEventListener("click", function() {loadDoc('fs_general.php')});
+	document.getElementById("reqReaction").addEventListener("click", function(){loadDoc('fs_reaction.php')});
+
