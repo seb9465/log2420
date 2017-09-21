9eeb6adf77f4c0e5b6501e64a68983183d4bf499
seb9465 <cado.94@me.com>
2017-09-20 20:31:42 -0400

    SC : Fixing stuff.

---
+++ tp1/factsage.js
@@ -0,0 +1,19 @@
+	
+/* Fait appel à une requette qui charge la page (change le contenu du tableau corp) */
+function loadDoc(url){
+    var xhttp = new XMLHttpRequest();
+    
+    xhttp.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById('corps').innerHTML = this.responseText;
+        }
+    };
+
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+	/* Fait l'appel à l'eveniment click afin d'appeler la fonction loadDoc qui affiche la page */
+	document.getElementById("reqGeneral").addEventListener("click", function() {loadDoc('fs_general.php')});
+	document.getElementById("reqReaction").addEventListener("click", function(){loadDoc('fs_reaction.php')});
+
