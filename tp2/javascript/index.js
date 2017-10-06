c5c0bf90d3035608d578cc9f1e8bedfd9a2dfe3a
seb9465 <cado.94@me.com>
2017-10-06 15:22:04 -0400

    SC : Ajout des marqueurs dans la map google.

--- tp2/javascript/index.js
+++ tp2/javascript/index.js
@@ -1,23 +1,9 @@
+var a = [];
+var nomStations = [];
+
+var coordonnees = [];
+
 /**
- * Fonction permettant l'affichage de la map Google.
- */
-function initMap() {
-    var uluru = {lat: 45.5087, lng: -73.554};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    }); 
-  }
-
-
- 
-
-
-  /**
    * Fonction permettant d'aller récupérer les informations sur le site
    * contenant les informations sur les stations BIXI.
    */
@@ -33,38 +19,50 @@ function initMap() {
       console.log("2. Data has been parse and placed in variables.");
       hi();
       console.log("3. Data has been put in the DataTables.");
+      initMap();
+      console.log("4. Init map.");
     } else {
       console.log("Error connecting to the server.");
     }
   };
   xmlhttp.send();
 
-var a = [];
-var nomStations = [];
-function hello() {
-  for(var i = 0 ; i < obj.length ; i++) {
-    var temp = [];
-    temp.push(obj[i].id);     //ID
-    temp.push(obj[i].s);      //Nom station
-    nomStations.push(obj[i].s);
-    temp.push(obj[i].ba);     //Vélos disponibles
-    temp.push(obj[i].da);     //Bornes disponibles
 
-    var etatBloque = "";
-    if(obj[i].b == true)      //État bloqué
+function hello() {
+  var temp = [];
+  var tempCoordonnees = [];
+  var etatBloque = "";
+  for(var i = 0 ; i < obj.length ; i++) {
+    temp = [];
+    tempCoordonnees = [];
+
+    temp.push(obj[i].id);             //ID
+    temp.push(obj[i].s);              //Nom station
+    temp.push(obj[i].ba);             //Vélos disponibles
+    temp.push(obj[i].da);             //Bornes disponibles
+
+    etatBloque = "";
+    if(obj[i].b == true)              //État bloqué
       etatBloque = "Oui";
     else
       etatBloque = "Non";
     temp.push(etatBloque);     
 
     var etatSuspendu = "";
-    if(obj[i].su == true)     //État suspendu
+    if(obj[i].su == true)             //État suspendu
       etatSuspendu = "Oui";
     else
       etatSuspendu = "Non";
-    temp.push(etatSuspendu);    
+    temp.push(etatSuspendu);   
+
+    nomStations.push(obj[i].s);       //Nom de stations
+
+    tempCoordonnees.push(obj[i].s);   //Nom de stations
+    tempCoordonnees.push(obj[i].la);  //Latitude
+    tempCoordonnees.push(obj[i].lo);  //Longitude 
 
     a.push(temp);
+    coordonnees.push(tempCoordonnees);
   }
 };
 
@@ -79,15 +77,51 @@ function hi() {
           { title : "Bornes disponibles" },
           { title : "État bloqué" },
           { title : "État suspendu" }
+        ],
+        columnDefs: [
+          { className: "dt-body-center" , "targets":[0,2,3,4,5] },    //Aligne au centre la 1ere colonne (ID).
+          { className: "dt-body-left" , "targets":[1] },              //Aligne à gauche la 2e colonne (nom des stations).
+          { className: "dt-head-center" , "targets":[0,2,3,4,5] },    //Aligne au centre la 1ere colonne (ID).
+          { className: "dt-head-left" , "targets":[1] }               //Aligne à gauche la 2e colonne (nom des stations).
         ]
     } );
   } );
 };
 
- /**
-   * Mettre une variable a la place de la liste de mot.
-   * Il faudra aller chercher les mots-cles dans la banque de donnees.
-   */
-  $( "#autocomplete" ).autocomplete({
-    source: nomStations
+
+  /**
+ * Fonction permettant l'affichage de la map Google.
+ */
+function initMap() {
+  var uluru = {lat: 45.5087, lng: -73.554};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: uluru
   });
+
+  
+  var marker, i;
+  var infowindow = new google.maps.InfoWindow();
+  for(i = 0 ; i < coordonnees.length ; i++) {
+    // Permet d'afficher plusieurs 'marker' dans la map.
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(coordonnees[i][1], coordonnees[i][2]),
+      map: map
+    }); 
+
+    //Permet d'afficher une bulle d'information lorsqu'on clique sur un marker.
+    google.maps.event.addListener(marker, 'click', (function(marker,i) {
+      return function() {
+        infowindow.setContent(coordonnees[i][0]);
+        infowindow.open(map,marker);
+      }
+    })(marker,i));
+  }
+};
+
+
+
+
+$( "#autocomplete" ).autocomplete({
+  source: nomStations
+});
