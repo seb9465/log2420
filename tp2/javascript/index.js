80ba1c998e7153790e071f397bc5fb5edcffac9b
unknown <andrei.catana@polymtl.ca>
2017-10-23 13:51:56 -0400

    fixing merging stuff

--- tp2/javascript/index.js
+++ tp2/javascript/index.js
@@ -1,6 +1,7 @@
 var infosDatatable = [];
 var nomStations = [];
 var coordonnees = [];
+let dict = {};
 var datatable;
 var mapGoogle;
 
@@ -20,7 +21,7 @@ var mapGoogle;
       createEmptyDataTable();
       initEmptyMap();
 
-      let dict = {};
+      //let dict = {};
       let dictNom = [];
       var mapMarker;
       var infowindow = new google.maps.InfoWindow();
@@ -37,20 +38,12 @@ var mapGoogle;
           'borneDisponible' : station.da
         }
         dict[newStation.nom] = newStation;
+		dictNom.push(newStation.nom);
         
         datatable.row.add(newStation).draw();
         mapMarker = addMapMarker(newStation);
         addListenerToMapMarker(infowindow, newStation, mapMarker);
         
-       
-
-        mapMarker = new google.maps.Marker({
-          position: new google.maps.LatLng(newStation.latitude, newStation.longitude),   //position (obligatoire) : emplacement du marqueur (latitude, longitude).
-          map: mapGoogle,                                                                 //map (facultatif) : Spécifie l'objet Map sur lequel placer le marqueur.
-          title: newStation.nom,                                                 //title (facultatif) : On mouse over, affiche un tooltip.            
-          animation: google.maps.Animation.DROP,                                    //animation (facultatif) : Animation du marker.
-          draggable: false,                                                         //draggable (falcultatif) : Empeche l'utilisateur de déplacer le marker.                                                          
-        }); 
       });
       initAutoComplete(dictNom);
     } else {
@@ -118,18 +111,19 @@ function initEmptyMap() {
 function initAutoComplete(stations) {
   $( "#autocomplete" ).autocomplete({
     minLength : 0,
-    source : stations
+    source : stations,
     //, success : ...
-	
-
+	/* traitement de leveniment select de l'autocomplete */
+	select: function( event, ui ) {
+					document.getElementById("idStation").innerHTML = dict[ui.item.value].id;
+					document.getElementById("velosDisponibles").innerHTML = dict[ui.item.value].veloDisponible;
+					document.getElementById("bloquee").innerHTML = dict[ui.item.value].etatBloque;
+					document.getElementById("bornesDisponibles").innerHTML = dict[ui.item.value].borneDisponible;
+					document.getElementById("suspendue").innerHTML = dict[ui.item.value].etatSuspendu;
+					document.getElementById("velosIndisponibles").innerHTML = dict[ui.item.value].id;
+					document.getElementById("horsService").innerHTML = dict[ui.item.value].id;
+					document.getElementById("bornesIndisponibles").innerHTML = dict[ui.item.value].id;
+               }
   });
 };
 
-$( "#autocomplete" ).on( "autocompleteselect", function( event, ui ) {
-	
-} );
-//function afficherInfoStation(station){
-	
-//};
-
-//document.getElementById('autocomplete').addEventListener("click", function(){afficherInfoStation(station)});
