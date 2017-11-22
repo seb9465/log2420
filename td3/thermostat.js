444e64ebb710be0b36a79afbad08715f8b86cd23
unknown <andrei.catana@polymtl.ca>
2017-11-22 13:28:13 -0500

    ajout des fichiers pour commencer

---
+++ td3/thermostat.js
@@ -0,0 +1,29 @@
+/* Extrait les valeurs produites dans la page Web et par le simulateur
+ * et déclanche l'affichage des valeurs dans la page
+*/
+
+
+/*Code jquery qui affiche un glisseur dans le conteneur ayant
+ *l'identifiant #thermostat, qui initalise sa position et ses valeurs
+ *par défaut et qui affiche la valeur sélectionnée dans un conteneur
+ *ayant l'identifiant #valeurThermostat
+ *
+ *Pour démarrer le chauffage, il faut cliquer le curseur de défilement
+ */
+/*********************Ne pas modifier***********************/
+ $(document).ready(function() {
+  $("#thermostat").slider(
+  {
+    orientation: 'vertical',
+    max: 40 ,
+    value:temperatureThermostat,
+    min: -10 ,
+    step: 1
+  });
+  $("#thermostat").slider({
+    change: function(event, ui) {
+      $("#tdValeurThermostat").text( ui.value );
+    }
+  });
+});
+/*********************Ne pas modifier***********************/
