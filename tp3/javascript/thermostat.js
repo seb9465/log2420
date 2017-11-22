951bad862b2b8e633ba145ab58a489a81896688d
unknown <andrei.catana@polymtl.ca>
2017-11-22 14:14:54 -0500

    regrouppement des fichers

--- tp3/javascript/thermostat.js
+++ tp3/javascript/thermostat.js
@@ -27,3 +27,20 @@
   });
 });
 /*********************Ne pas modifier***********************/
+
+/*************************** Slider ************************/
+    $( function() {
+        $("#slider-vertical").slider({
+            orientation: "vertical",
+            range: "min",
+            min: 0,
+            max: 100,
+            value: 60,
+            slide: function (event, ui) {
+                $("#amount").val(ui.value);
+            }
+        });
+    $( "#amount" ).val( $( "#slider-vertical" ).slider( "value" ) );
+  } );
+/*************************** Fin Slider **********************/
+
